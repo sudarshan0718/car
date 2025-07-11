7aee3205133cfb399980e6ad400dce151a1774e6
GitDemo <sudarshanchalaki@gmail.com>
2025-07-11 18:14:33 +0530

    i need love

---
+++ love.java
@@ -0,0 +1,9 @@
+package car;
+public class love {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
